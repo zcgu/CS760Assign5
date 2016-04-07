b3777f34a8704660dc6e6923a4004e3f189b95df
zhicheng <zchenggu@gmail.com>
2016-04-06 21:16:53 -0500

    done

--- neural_net/neural_model.py
+++ neural_net/neural_model.py
@@ -5,34 +5,81 @@ def sigmoid(x):
     return 1. / (1. + math.pow(math.e, - x))
 
 
-class Connection:
-    def __init__(self):
-        self.weight = 0.1
-
-
 class Network:
-    def __init__(self, input_dimension, class_label):
+    def __init__(self, input_dimension, class_label, learning_rate):
         self.input_dimension = input_dimension
         self.class_label = class_label
+        self.learning_rate = learning_rate
 
-        self.connection_list = []
+        self.weight_list = []
         for i in range(0, input_dimension + 1):
-            self.connection_list.append(Connection())
+            self.weight_list.append(0.1)
 
     def train(self, row):
-        input_row = row[:len(row) - 1]
+        input_row = row[:-1]
         input_row.append(1.)
 
         if row[-1].lower() == self.class_label[0].lower():
-            label = 0
+            label = 0.
         else:
-            label = 1
+            label = 1.
 
-        print label
+        # print label
 
-        output = 0.
+        sum_input = 0.
         for i in range(0, len(input_row)):
-            output += input_row[i] * self.connection_list[i].weight
+            sum_input += input_row[i] * self.weight_list[i]
+        output = sigmoid(sum_input)
 
-        print output
-        print output - label
+        # print output
+        # print output - label
+
+        delta = output * (1. - output) * (label - output)
+        for i in range(0, len(self.weight_list)):
+            self.weight_list[i] += self.learning_rate * delta * input_row[i]
+
+        # print self.weight_list
+
+    def predict(self, row):
+        input_row = row[:-1]
+        input_row.append(1.)
+
+        sum_input = 0.
+        for i in range(0, len(input_row)):
+            sum_input += input_row[i] * self.weight_list[i]
+        output = sigmoid(sum_input)
+
+        if output < 0.5:
+            predict_label = self.class_label[0]
+        else:
+            predict_label = self.class_label[1]
+
+        return predict_label, output
+
+
+    def cross_validation(self, stratified_data_rows, num_epochs):
+        output = {}
+
+        for i in range(0, len(stratified_data_rows)):
+            for j in range(0, self.input_dimension + 1):
+                self.weight_list[j] = 0.1
+
+            traning_rows = []
+            testing_rows = []
+
+            for row in stratified_data_rows[i]:
+                testing_rows.append(row)
+            for j in range(0, len(stratified_data_rows)):
+                if j != i:
+                    for row in stratified_data_rows[j]:
+                        traning_rows.append(row)
+
+            for epoch in range(0, num_epochs):
+                for row in traning_rows:
+                    self.train(row)
+
+            for row in testing_rows:
+                predict_label, confidence_of_prediction = self.predict(row)
+                output[str(row)] = (i, predict_label, row[-1], confidence_of_prediction)
+
+        return output
--- neuralnet.py
+++ neuralnet.py
@@ -12,7 +12,14 @@ stratified_data_rows = n_fold_stratified(num_folds, data_rows, class_label)
 # print data_rows[0]
 # print class_label
 
-network = Network(len(data_rows) - 1, class_label)
+network = Network(len(data_rows[0]) - 1, class_label, learning_rate)
 
+# print stratified_data_rows[0][0]
 
+output = network.cross_validation(stratified_data_rows, num_epochs)
 
+# print output
+
+for row in data_rows:
+    print str(output[str(row)][0]) + ' ' + output[str(row)][1].encode("ascii") \
+          + ' ' + output[str(row)][2].encode("ascii") + ' ' + str(output[str(row)][3])
